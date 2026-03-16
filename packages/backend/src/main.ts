99eed7fbdd08373b6527e271ab29bacfa38c7a83
Suryakand Shinde <suryakand.shinde@cognizant.com>
2026-03-16 19:22:49 -0400

    Added backend project to monorepo

---
+++ packages/backend/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = process.env.PORT || 3000;
+  const app = await NestFactory.create(AppModule);
+  
+  console.log(`Server is running on port ${port}`);
+  await app.listen(port);
+}
+bootstrap();
